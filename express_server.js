5cd2ca6225fd84f86b93362d165045c754f02a1a
namraaslam <namra.aslam@ucalgary.ca>
2023-02-20 22:49:44 -0700

    added views and partials folder for header abd updated express and url_index file

--- express_server.js
+++ express_server.js
@@ -15,6 +15,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
