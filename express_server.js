4157f7065b54ab44f58a3ed3ab35c4a696ea33dc
namraaslam <namra.aslam@ucalgary.ca>
2023-02-25 16:40:37 -0700

    implemented logout client and server logic in _header.ejs and express_Server.js

--- express_server.js
+++ express_server.js
@@ -41,8 +41,15 @@ app.post("/urls/:id", (req, res) => {
 
 app.post("/login", (req, res) => {
   console.log("Login with username", req.body)
-  const username = req.body.username
-  res.cookie("username", username)
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls")
+});
+
+app.post("/logout", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.clearCookie("username", username);
   res.redirect("/urls")
 });
 
